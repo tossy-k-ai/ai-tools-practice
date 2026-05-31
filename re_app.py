2b3f7082c108d7a0bc42f24464c70f43953b28b7
Toshihiro <konichiwa0@gmail.com>
2026-05-31 14:53:55 +0900

    git add re_app.py

--- re_app.py
+++ re_app.py
@@ -2,30 +2,41 @@ import csv
 
 expenses = []
 
+class Customer:
+    def __init__(self,company,name,address):
+        self.company = company
+        self.name = name
+        self.address = address
+    def show(self):
+        print(f"会社:{self.company}")
+        print(f"担当者:{self.name}")
+        print(f"連絡先:{self.address}")
+
+
 def add_data(): 
         company = input("company")
         name = input("name")
         address = input("address")
 
-        expenses.append({"company":company,"name":name,"address":address})
+        expenses.append(Customer(company,name,address))
 
 def save_data():        
     with open("data2.csv","w",newline="",encoding="utf-8")as f:
             writer = csv.DictWriter(f,fieldnames=["company","name","address"])
-
+            rows = [{"company":e.company,"name":e.name,"address":e.address} for e in expenses]
             writer.writeheader()
-            writer.writerows(expenses)
+            writer.writerows(rows)
+
 def show_data():
         for e in expenses:
-            print(e["company"])
-            print(e["name"])
-            print(e["address"])
+            e.show()
 
 try:
     with open("data2.csv","r",encoding="utf-8")as f:
         reader=csv.DictReader(f)
         for e in reader:
-            expenses.append(e)
+            expenses.append(Customer(e["company"],e["name"],e["address"]))
+                                       
 except FileNotFoundError:
     pass
 
